a9e5cc31d90ae6ade0793f4101461bcab384436c
Shyamal <shyamalgs@gmail.com>
2020-09-21 14:30:29 -0400

    Game class

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
@@ -53,6 +53,12 @@ namespace CrossPlatformDesktopProject
             // TODO: Unload any non ContentManager content here
         }
 
+        public void chooseSprite(int entry)
+        {
+
+        }
+
+
         /// <summary>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
