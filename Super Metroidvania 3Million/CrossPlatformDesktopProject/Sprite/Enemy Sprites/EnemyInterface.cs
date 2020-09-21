260cc0c38864cefd29ca9de794e94d37350ecf24
Alex Nguyen <nguyen.1637@buckeyemail.osu.edu>
2020-09-21 19:04:24 -0400

    Implemented enemy interface

---
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Sprite/Enemy Sprites/EnemyInterface.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformDesktopProject.Sprite.Enemy_Sprites
+{
+    interface EnemyInterface
+    {
+        public void Attack();
+    }
+}
