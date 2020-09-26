9e5854ade80401510356b50aa37cd25b518c13e5
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-09-26 17:08:44 -0400

    Starting player interface and sprite methods

---
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/PlayerInterface.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformDesktopProject.Sprite.Enemy_Sprites
+{
+    interface PlayerInterface
+    {
+        void Attack();
+        void Special();
+        void Jump();
+        void Crouch();
+        void RunRight();
+        void RunLeft();
+        
+
+    }
+}
