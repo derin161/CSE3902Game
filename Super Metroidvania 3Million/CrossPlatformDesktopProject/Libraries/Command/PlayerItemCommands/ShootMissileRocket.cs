f9726bc0733585aed5a5bf145c36ef983d36cd58
Nyigel <nkspann@gmail.com>
2020-09-28 20:09:26 -0400

    ShootCommands

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Command/PlayerItemCommands/ShootMissileRocket.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Command/PlayerItemCommands/ShootMissileRocket.cs
@@ -11,13 +11,13 @@ using System.Threading.Tasks;
 namespace CrossPlatformDesktopProject.Libraries.Command.PlayerItemCommands
 {
     //Author: Nyigel Spann
-    class ShootBeam : ICommand
+    class ShootMissileRocket : ICommand
     {
         private PlayerSprite samus;
         private IFactory factory;
         private float speed = 1;
 
-        public ShootBeam(PlayerSprite player, IFactory factory) {
+        public ShootMissileRocket(PlayerSprite player, IFactory factory) {
             samus = player;
             this.factory = factory;
         }
@@ -29,17 +29,8 @@ namespace CrossPlatformDesktopProject.Libraries.Command.PlayerItemCommands
                 direction = new Vector2(-speed, 0);
             }
             Vector2 location = new Vector2(samus.Location.X, samus.Location.Y);
-            if (samus.wave)
-            {
-                factory.CreateWaveBeam(location, direction, samus.elong, samus.ice);
-            }
-            else if (samus.ice)
-            {
-                factory.CreateIceBeam(location, direction, samus.elong);
-            }
-            else { //Power beam
-                factory.CreatePowerBeam(location, direction, samus.elong);
-            } 
+            
+            
             
             
         }
