c58906f16ecf7952636c5d3028e39ab22d4f564f
wrfloyd15 <wrfloyd15@gmail.com>
2020-10-01 15:55:34 -0400

    added IsDead to enemies

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Enemy Sprites/Geega.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Enemy Sprites/Geega.cs
@@ -52,6 +52,9 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.EnemySprites
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
             spriteBatch.End();
         }
-        
+        public Boolean IsDead()
+        {
+            return false;
+        }
     }
 }
