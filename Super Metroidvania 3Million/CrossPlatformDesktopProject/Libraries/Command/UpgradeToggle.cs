cbe0bf6c10bc9ad413a0ff14478f619f9346ac8f
Nyigel <nkspann@gmail.com>
2020-10-05 00:00:02 -0400

    UpgradeToggle

---
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Command/UpgradeToggle.cs
@@ -0,0 +1,25 @@
+﻿using CrossPlatformDesktopProject.Libraries.Sprite.Items;
+using CrossPlatformDesktopProject.Libraries.Sprite.PlayerSprite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformDesktopProject.Libraries.Command
+{
+    class UpgradeToggle : ICommand
+    {
+
+        private PlayerSprite.UpgradeType type;
+        private PlayerSprite samus;
+        public UpgradeToggle(UpgradeType type, PlayerSprite samus) {
+            this.type = type;
+            this.samus = samus;
+        }
+        public void Execute()
+        {
+            samus.Upgrade(type);
+        }
+    }
+}
