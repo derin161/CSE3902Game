b08d518de6058b8ce6282530780e745af4d319db
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-10-05 20:36:24 -0400

    tweaks

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Player Sprites/PlayerSprite.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Player Sprites/PlayerSprite.cs
@@ -29,7 +29,7 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.PlayerSprite
         }
         public HealthState currentHealthState;
 
-        public Vector2 HealthPosition = new Vector2(20, 20);
+        public Vector2 HealthPosition = new Vector2(20, 90);
 
         public bool dead = false;
         public int currentHealth = 100;
@@ -150,7 +150,6 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.PlayerSprite
         {
             this.maxHealth = maxHealth;
         }
-
         public void UpdateHealthState()
         {
             float percent = (float)currentHealth / (float)maxHealth;
