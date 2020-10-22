64bcc76c8a33af47775a8c3cc3a2b72b1d92eafc
wrfloyd15 <wrfloyd15@gmail.com>
2020-10-21 23:57:43 -0400

    Added more collision checks to collisionDetector and collisionHandler

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Detector/CollisionDetector.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Detector/CollisionDetector.cs
@@ -10,16 +10,18 @@ namespace CrossPlatformDesktopProject.Libraries.Collision
         List<IGameObject> enemyList;
         List<IGameObject> blockList;
         List<IGameObject> projectileList;
+        List<IGameObject> itemList;
         bool collision;
         CollisionHandler handler;
 
-        public CollisionDetector(IPlayer Player, IContainer enemyContainer, IContainer blockContainer, IContainer projectileContainer)
+        public CollisionDetector(IPlayer Player, IContainer enemyContainer, IContainer blockContainer, IContainer projectileContainer, IContainer itemContainer)
         {
 
             player = Player;
             enemyList = enemyContainer.GetList();
             blockList = blockContainer.GetList();
             projectileList = projectileContainer.GetList();
+            itemList = itemContainer.GetList();
             handler = new CollisionHandler();
             
         }
@@ -56,27 +58,69 @@ namespace CrossPlatformDesktopProject.Libraries.Collision
                 }
             }
 
+            //Check if the player is colliding with any items
+            foreach (IGameObject item in itemList)
+            {
+                collision = CheckCollisions(player, item);
+                if (collision)
+                {
+                    handler.PlayerItemCollision(player, item);
+                }
+            }
+
+            //check if the enemies are colliding with any blocks
+            foreach (IGameObject enemy in enemyList)
+            {
+                foreach(IGameObject block in blockList)
+                {
+                    collision = CheckCollisions(enemy, block);
+                    if (collision)
+                    {
+                        handler.EnemyBlockCollision(enemy, block);
+                    }
+                }
+            }
+
+            //check if the enemies are colliding with any blocks
+            foreach (IGameObject projectile in projectileList)
+            {
+                foreach (IGameObject block in blockList)
+                {
+                    collision = CheckCollisions(projectile, block);
+                    if (collision)
+                    {
+                        handler.ProjectileBlockCollision(projectile, block);
+                    }
+                }
+            }
+
+            //check if the enemies are colliding with any blocks
+            foreach (IGameObject projectile in projectileList)
+            {
+                foreach (IGameObject enemy in enemyList)
+                {
+                    collision = CheckCollisions(projectile, enemy);
+                    if (collision)
+                    {
+                        handler.ProjectileEnemyCollision(projectile, enemy);
+                    }
+                }
+            }
+
+
             /*Collisions that we DO NOT need to check for:
             - block - block (Static, will never collide)
             - enemy - enemy (fine if they pass through each other)
+            - projectiles - projectiles
             */
-
-            /*Collisions that we MIGHT need to check for:
-            - enemy - block
-            - projectile - block
-            - projectile - enemy
-
-             */
 
 
         }
 
         private bool CheckCollisions(IGameObject obj1, IGameObject obj2)
         {
-            //TODO: insert collision detection logic here
-
-            //Use obj.Space rectangles to determine collisions (follow slides)
-            return false; //placeholder
+            //return obj1.Space.Intersects(obj2.Space);
+            return false;
         }
     }
 }
--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Handler/CollisionHandler.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Handler/CollisionHandler.cs
@@ -12,17 +12,39 @@ namespace CrossPlatformDesktopProject.Libraries.Collision
 
         public void PlayerEnemyCollision(IPlayer player, IGameObject enemy)
         {
-
+            //Do amount of damage spcified by enemy to player
         }
 
         public void PlayerBlockCollision(IPlayer player, IGameObject block)
         {
-
+            //Determine the direction that the player came from and push the player back out of the block
         }
 
         public void PlayerProjectileCollision(IPlayer player, IGameObject projectile)
         {
+            //Do damage specifiied by the projectile to the player
+            //This method should only be called for Kraid's projectiles, no other enemies have any
+        }
 
+        public void EnemyBlockCollision(IGameObject enemy, IGameObject block)
+        {
+            //Same as player block collisions
+            //Determine direction that enemy came from and push them back out of the block
+        }
+
+        public void ProjectileBlockCollision(IGameObject projectile, IGameObject block)
+        {
+            //Kill the projectile
+        }
+
+        public void ProjectileEnemyCollision(IGameObject projectile, IGameObject enemy)
+        {
+            //Do amount of damage to enemies specified by the projectile 
+        }
+
+        public void PlayerItemCollision(IPlayer player, IGameObject item)
+        {
+            //Do amount of damage to enemies specified by the projectile 
         }
     }
 }
