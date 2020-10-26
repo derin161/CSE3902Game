73817f747e703d6b0ce1da971ba5ad001bc61a88
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-10-26 18:10:54 -0400

    LoadCsv implemented

---
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LoadCsv.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Xna.Framework;
+using CrossPlatformDesktopProject.Libraries.Container;
+using CrossPlatformDesktopProject.Libraries.Sprite.Blocks;
+
+namespace CrossPlatformDesktopProject.Libraries.CSV
+{
+    public class LoadCSV
+    {
+        public void Load()
+        {
+            using (TextFieldParser parser = new TextFieldParser(@"~\Levels\StartingLevel.csv"))
+            {
+                int column = 0;
+                int row;
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    row = 0;
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields)
+                    {
+                        Vector2 location;
+                        IBlock block;
+
+                        //TODO: Process field
+                        switch (field)
+                        {
+                            case "BlueBrickBlock":
+                                location = new Vector2(row*32, column*32);
+                                block = new BlueBrickBlock(location);
+                                GameObjectContainer.Instance.Add(block);
+                                break;
+
+                            case "BushBlockBlue":
+                                location = new Vector2(row * 32, column * 32);
+                                block = new BushBlockBlue(location);
+                                GameObjectContainer.Instance.Add(block);
+                                break;
+
+                            case "TubeBlockBlue":
+                                location = new Vector2(row * 32, column * 32);
+                                block = new TubeBlockBlue(location);
+                                GameObjectContainer.Instance.Add(block);
+                                break;
+
+                            default:
+                                break;
+                        }
+                        row++;
+                    }
+                    column++;
+                }
+            }
+        }
+    }
+}
--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Blocks/BushBlockBlue.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Blocks/BushBlockBlue.cs
@@ -19,7 +19,7 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.Blocks
         private bool isDead = false;
 
 
-        public BushBlockBlue(Vector2 initialLocation, bool isMovingRight)
+        public BushBlockBlue(Vector2 initialLocation)
         {
             this.initialLocation = initialLocation;
             Location = initialLocation;
@@ -38,7 +38,6 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.Blocks
         {
 
             //Update position and space
-            Location = new Vector2(); //Until we implement the map and figure out how location is being updated
             Space = new Rectangle((int)Location.X, (int)Location.Y, Space.Width, Space.Height);
 
             bool collision = false; //temp var til collisions are added
