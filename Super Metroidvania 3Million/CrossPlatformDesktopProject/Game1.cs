e5ac93200f5d84817b50ac8a02bf216190a15f58
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-10-26 20:44:27 -0400

    LevelOne Commit + Merge

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
@@ -60,7 +60,7 @@ namespace CrossPlatformDesktopProject
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin();
 
