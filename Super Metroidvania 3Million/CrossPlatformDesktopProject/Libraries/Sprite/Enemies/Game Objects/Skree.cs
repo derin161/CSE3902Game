7cacff5aeace412aba516e102c5b74477094ab8b
wrfloyd15 <wrfloyd15@gmail.com>
2020-10-26 21:45:43 -0400

    Added changeDirection to enemies

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Enemies/Game Objects/Skree.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Enemies/Game Objects/Skree.cs
@@ -71,6 +71,10 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.EnemySprites
         {
             stateMachine.MoveDown();
         }
+        public void ChangeDirection()
+        {
+            stateMachine.changeDirection();
+        }
         public void Freeze()
         {
             stateMachine.Freeze();
