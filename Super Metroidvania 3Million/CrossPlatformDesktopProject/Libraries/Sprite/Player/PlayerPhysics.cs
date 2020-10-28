9a46008f01e096c9707829867303c20ee300362a
Nyigel <nkspann@gmail.com>
2020-10-28 13:24:27 -0400

    Added MoveLeft and MoveRight to PlayerPhysics.

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Player/PlayerPhysics.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Player/PlayerPhysics.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using CrossPlatformDesktopProject.Libraries.Sprite.Blocks;
 using Microsoft.Xna.Framework;
 
 namespace CrossPlatformDesktopProject.Libraries.Sprite.Player
@@ -8,7 +9,9 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.Player
     {
         private Vector2 acceleration = new Vector2(0, 0.5f);
         private Vector2 velocity = new Vector2(0, 0);
-        private Vector2 maxVelocity;
+        private float maxFallVelocity = 5;
+        private float maxHorizontalVelocity = 5;
+        private float horizontalRunAcceleration = 0.5f;
         private Samus player;
 
         public PlayerPhysics(Samus player) {
@@ -17,12 +20,27 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.Player
 
         public void Update() {
             player.position = Vector2.Add(player.position, velocity);
-            player.space = new Rectangle((int)player.position.X, (int)player.position.Y, 64, 64);
             velocity = Vector2.Add(velocity, acceleration);
+            if (velocity.Y > maxFallVelocity) {
+                velocity = new Vector2(velocity.X, maxFallVelocity);
+            }
+            if (velocity.X > maxHorizontalVelocity) {
+                velocity = new Vector2( maxHorizontalVelocity, velocity.Y);
+            } else if (velocity.X < -1 * maxHorizontalVelocity) {
+                velocity = new Vector2(-1 * maxHorizontalVelocity, velocity.Y);
+            }
         }
 
         public void Break() {
-            this.velocity = new Vector2(0, 0);
+            this.velocity = Vector2.Zero;
+        }
+
+        public void MoveRight() {
+            this.velocity = Vector2.Add(this.velocity, new Vector2(horizontalRunAcceleration, 0));
+        }
+
+        public void MoveLeft() {
+            this.velocity = Vector2.Add(this.velocity, new Vector2(horizontalRunAcceleration * -1, 0));
         }
 
 
