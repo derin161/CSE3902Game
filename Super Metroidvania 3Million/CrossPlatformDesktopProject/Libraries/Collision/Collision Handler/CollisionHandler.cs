c91a12ebf163a3de5ad26dede593c110a306487d
wrfloyd15 <wrfloyd15@gmail.com>
2020-11-09 11:18:05 -0500

    Added player-block and player-enemy collision classes

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Handler/CollisionHandler.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Collision/Collision Handler/CollisionHandler.cs
@@ -19,64 +19,12 @@ namespace CrossPlatformDesktopProject.Libraries.Collision
 
         public void PlayerEnemyCollision(IPlayer player, IEnemy enemy)
         {
-            new EnemyDamagePlayerCommand(player, enemy).Execute();
+            new PlayerEnemyCollision().HandleCollision(player, enemy);
         }
 
         public void PlayerBlockCollision(IPlayer player, IBlock block, Rectangle collisionZone)
         {
-            Samus sam = ((Samus)player);
-            if (block is IDoorBlock) //&& ((IDoorBlock)block).isOpen())
-            {
-                if (collisionZone.X >= 240)
-                {
-                    LevelStatePattern.Instance.SwitchLevel(LevelStatePattern.Door.right);
-                }
-                else
-                {
-                    LevelStatePattern.Instance.SwitchLevel(LevelStatePattern.Door.left);
-                }
-            }
-
-            else
-            {
-                //Use collisionZone to determine LEFT/RIGHT or TOP/BOTTOM collision.
-                if (collisionZone.Height > collisionZone.Width)
-                { //LEFT/RIGHT collision
-                    sam.Physics.HortizontalBreak();
-                    if (player.SpaceRectangle().X < block.SpaceRectangle().X)
-                    { //LEFT Collision
-                      //sam.position = new Vector2(sam.position.X - collisionZone.Width, sam.position.Y);
-                        sam.x -= collisionZone.Width;
-                        sam.space = new Rectangle(sam.space.X - collisionZone.Width, sam.space.Y, sam.space.Width, sam.space.Height);
-                    }
-                    else
-                    { //RIGHT Collision 
-                      //sam.position = new Vector2(sam.position.X + collisionZone.Width, sam.position.Y);
-                        sam.x += collisionZone.Width;
-                        sam.space = new Rectangle(sam.space.X + collisionZone.Width, sam.space.Y, sam.space.Width, sam.space.Height);
-                    }
-                }
-                else
-                { //TOP/BOTTOM collision
-                    sam.Physics.VerticalBreak();
-                    if (player.SpaceRectangle().Y < block.SpaceRectangle().Y)
-                    { //TOP Collision
-                      //sam.position = new Vector2(sam.position.X, sam.position.Y - collisionZone.Height);
-                        if (sam.Jumping){
-                            sam.Jumping = false;
-                            sam.Idle();
-                        }
-                        sam.y -= collisionZone.Height;
-                        sam.space = new Rectangle(sam.space.X, sam.space.Y - collisionZone.Height, sam.space.Width, sam.space.Height);
-                    }
-                    else
-                    { //BOTTOM Collision 
-                      //sam.position = new Vector2(sam.position.X, sam.position.Y + collisionZone.Height);
-                        sam.y += collisionZone.Height;
-                        sam.space = new Rectangle(sam.space.X, sam.space.Y + collisionZone.Height, sam.space.Width, sam.space.Height);
-                    }
-                }
-            }
+            new PlayerBlockCollision().HandleCollision(player, block, collisionZone);
         }
 
         public void PlayerProjectileCollision(IPlayer player, IProjectile projectile)
