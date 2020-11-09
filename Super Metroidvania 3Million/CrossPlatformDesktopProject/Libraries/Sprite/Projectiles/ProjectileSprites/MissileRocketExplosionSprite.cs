f9b37fd7fa1f47c65c1ef853e33ef0ece44631a0
Nyigel <nkspann@gmail.com>
2020-11-09 13:43:12 -0500

    Added MissileRocketExplosion Animation and Fixed MissileRocket direction.

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Projectiles/ProjectileSprites/MissileRocketExplosionSprite.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/Sprite/Projectiles/ProjectileSprites/MissileRocketExplosionSprite.cs
@@ -1,26 +1,25 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CrossPlatformDesktopProject.Libraries.Sprite.Blocks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 
 namespace CrossPlatformDesktopProject.Libraries.Sprite.Projectiles
 {
     //Author: Nyigel Spann
     //This class isn't needed until collisions are added.
-    class MissileRocketExplosion : ISprite
+    class MissileRocketExplosionSprite : ISprite
     {
         enum AnimationPos { 
             Right, BottomRight, Bottom, BottomLeft, Left, UpperLeft, Upper, UpperRight
         }
 
-        private Dictionary<AnimationPos, Vector2> explosionAnimationPairs; //Contains animationPos mapped to their relative positions (to explosion origin)
-        private Vector2 explosionOrigin;
+        private Dictionary<AnimationPos, Vector2> explosionAnimationPairs = new Dictionary<AnimationPos, Vector2>(); //Contains animationPos mapped to their relative positions (to explosion origin)
         private Texture2D texture;
-        private int time = 0;
-        private int endTime = 500;
-        private bool isDead = false;
-        public MissileRocketExplosion(Texture2D texture, Vector2 location) {
+        private MissileRocketExplosion projectile;
+        public MissileRocketExplosionSprite(Texture2D texture, MissileRocketExplosion mre) {
             this.texture = texture;
-            explosionOrigin = location;
+            projectile = mre;
 
             explosionAnimationPairs.Add(AnimationPos.Right, new Vector2(1, 0));
             explosionAnimationPairs.Add(AnimationPos.Bottom, new Vector2(0, 1));
@@ -33,51 +32,55 @@ namespace CrossPlatformDesktopProject.Libraries.Sprite.Projectiles
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            if (time < endTime)
-            {
                 foreach (KeyValuePair<AnimationPos, Vector2> entry in explosionAnimationPairs)
                 {
                     Rectangle recSrc = new Rectangle(26, 0, 8, 8); //Explosion corner texture
                     if (entry.Value.X == 0 || entry.Value.Y == 0) {
                         recSrc = new Rectangle(35, 0, 8, 8); //Explosion side texture
                     }
-                    Rectangle recDest = new Rectangle((int)(explosionOrigin.X + entry.Value.X), (int)(explosionOrigin.Y + entry.Value.Y), 8, 8);
+                    Rectangle recDest = new Rectangle((int)(projectile.Location.X + entry.Value.X), (int)(projectile.Location.Y + entry.Value.Y), 8, 8);
                     rotateAndDraw(spriteBatch, entry.Key, recDest, recSrc);
                 }
-            }
         }
 
         public void Update(GameTime gameTime)
         {
-            time += gameTime.ElapsedGameTime.Milliseconds;
+            
 
             Vector2 right = new Vector2(1, 0);
             Vector2 left = new Vector2(-1, 0);
             Vector2 up = new Vector2(0, -1);
             Vector2 down = new Vector2(0, 1);
-
             //Adjust all x and y relative positions to push them further from the explosion origin
-            foreach (KeyValuePair<AnimationPos, Vector2> entry in explosionAnimationPairs) {
-                if (entry.Value.X > 0) {
-                    explosionAnimationPairs[entry.Key] = Vector2.Add(entry.Value, right);
-                } else if (entry.Value.X < 0) {
-                    explosionAnimationPairs[entry.Key] = Vector2.Add(entry.Value, left);
+            for (int i = 0; i < 7; i++) {
+                /*This is kind've stupid but allows me to change to dictionary during iteration whereas a foreach loop wouldn't
+                  This cast is guaranteed to succeed as well as the dictionary indexing. */
+                AnimationPos key = (AnimationPos) i;
+                Vector2 pos = explosionAnimationPairs[key];
+                if (pos.X > 0)
+                {
+                    pos = Vector2.Add(pos, right);
+                }
+                else if (pos.X < 0)
+                {
+                    pos = Vector2.Add(pos, left);
                 }
 
-                if (entry.Value.Y > 0)
+                if (pos.Y > 0)
                 {
-                    explosionAnimationPairs[entry.Key] = Vector2.Add(entry.Value, down);
+                    pos = Vector2.Add(pos, down);
                 }
-                else if (entry.Value.Y < 0)
+                else if (pos.Y < 0)
                 {
-                    explosionAnimationPairs[entry.Key] = Vector2.Add(entry.Value, up);
+                    pos = Vector2.Add(pos, up);
                 }
+                explosionAnimationPairs[key] = pos;
+                Console.WriteLine(explosionAnimationPairs + " = " + pos);
             }
+
         }
 
-        public bool IsDead() {
-            return isDead;
-        }
+        
 
         private void rotateAndDraw(SpriteBatch sb, AnimationPos pos, Rectangle dest, Rectangle src) {
             Vector2 center = new Vector2(src.Width/2, src.Height / 2);
