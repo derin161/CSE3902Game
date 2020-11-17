d91f307216949dcb6feefeacbd87a01ccfa3470b
Alex Nguyen <nguyen.1637@buckeyemail.osu.edu>
2020-11-16 23:53:13 -0500

    misc

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameObjects/Items/Sprites/UpgradeItemSprite.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameObjects/Items/Sprites/UpgradeItemSprite.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-
 namespace CrossPlatformDesktopProject.Libraries.Sprite.Items.Sprites
 {
     class UpgradeItemSprite : ISprite
