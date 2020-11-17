26b4ceb4cbd76c172661d31fc760df794b173e06
wrfloyd15 <wrfloyd15@gmail.com>
2020-11-16 21:41:28 -0500

    Added draw methods to game state

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameStates/States/PausedState.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameStates/States/PausedState.cs
@@ -3,7 +3,7 @@ using CrossPlatformDesktopProject.Libraries.Sprite.Items;
 using Microsoft.Xna.Framework;
 using System;
 using Microsoft.Xna.Framework.Input;
-
+using Microsoft.Xna.Framework.Graphics;
 
 namespace CrossPlatformDesktopProject.Libraries.Container
 {
@@ -13,6 +13,10 @@ namespace CrossPlatformDesktopProject.Libraries.Container
         public void Update(GameTime gameTime)
         {
 
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            GameObjectContainer.Instance.Draw(spriteBatch);
         }
 
     }
