2415383fbe8e403ef5e095e5b6a884e7e618e1bd
wrfloyd15 <wrfloyd15@gmail.com>
2020-11-16 22:14:01 -0500

    Improved pause state

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Game1.cs
@@ -63,6 +63,7 @@ namespace CrossPlatformDesktopProject
             camera.CameraPosition = new Vector2(camera.Focus.SpaceRectangle().X - camera.Viewport.Width / camera.Zoom / 2, camera.CameraPosition.Y);
             SoundManager.Instance.LoadAllSounds(Content);
             keyboard = new KeyboardController(this);
+            GameStateMachine.Instance.RegisterKeyboardController(keyboard);
             currentLevel.Initialize();
         }
 
