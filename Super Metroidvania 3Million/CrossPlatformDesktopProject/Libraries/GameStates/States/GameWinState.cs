da7865d88a98daa190216f580acb3bc70299537c
wrfloyd15 <wrfloyd15@gmail.com>
2020-11-30 17:46:55 -0500

    Added some text to game win screen, still need to fix positioning

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameStates/States/GameWinState.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameStates/States/GameWinState.cs
@@ -1,15 +1,18 @@
 ﻿using CrossPlatformDesktopProject.Libraries.Audio;
 using CrossPlatformDesktopProject.Libraries.Container;
+using CrossPlatformDesktopProject.Libraries.SFactory;
 using CrossPlatformDesktopProject.Libraries.Sprite.Items;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+
 
 namespace CrossPlatformDesktopProject.Libraries.Container
 {
     //Author: Will Floyd
     public class GameWinState : IGameState
     {
+
         public void Update(GameTime gameTime)
         {
             SoundManager.Instance.Songs.PlayDarudeSandstorm();
@@ -18,6 +21,9 @@ namespace CrossPlatformDesktopProject.Libraries.Container
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.GraphicsDevice.Clear(Color.Black);
+            SpriteFont font = MenuSpriteFactory.Instance.DefaultFont;
+            spriteBatch.DrawString(font, "You Win!!!", new Vector2(100, 200), Color.White);
+            spriteBatch.DrawString(font, "Press 'r' To Restart", new Vector2(75, 300), Color.White);
         }
 
     }
