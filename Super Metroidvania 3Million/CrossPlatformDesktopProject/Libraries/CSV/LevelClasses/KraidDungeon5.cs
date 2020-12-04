b658374f887aeddc03c42e4c44e09cb2536713a6
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-12-04 18:57:49 -0500

    Updates to Levels

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/KraidDungeon5.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/KraidDungeon5.cs
@@ -12,21 +12,21 @@ namespace SuperMetroidvania5Million.Libraries.CSV
 
         public void LeftDoor(Game1 game)
         {
-            LoadCsv.Instance.Load("KraidDungeon4.csv", new Vector2(1400, 192), game);
-            LevelStatePattern.Instance.state = new KraidDungeon4();
+            // Do nothing - door does not exist
         }
         public void RightDoor(Game1 game)
         {
-            LoadCsv.Instance.Load("KraidDungeon6.csv", new Vector2(64, 224), game);
-            LevelStatePattern.Instance.state = new KraidDungeon6();
+            // Do nothing - door does not exist
         }
         public void TopLeftDoor(Game1 game)
         {
-            // Do nothing - door does not exist
+            LoadCsv.Instance.Load("KraidDungeon4.csv", new Vector2(1400, 192), game);
+            LevelStatePattern.Instance.state = new KraidDungeon4();
         }
         public void TopRightDoor(Game1 game)
         {
-            // Do nothing - door does not exist
+            LoadCsv.Instance.Load("KraidDungeon6.csv", new Vector2(64, 224), game);
+            LevelStatePattern.Instance.state = new KraidDungeon6();
         }
         public void BottomLeftDoor(Game1 game)
         {
