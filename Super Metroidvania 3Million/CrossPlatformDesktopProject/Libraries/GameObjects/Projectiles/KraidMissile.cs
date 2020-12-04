8b016b7d6946f7b10664ee82593dd2bccc3be6e2
Nyigel <nkspann@gmail.com>
2020-12-03 21:19:55 -0500

    Stopped projectiles from dying when leaving the screen.

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameObjects/Projectiles/KraidMissile.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/GameObjects/Projectiles/KraidMissile.cs
@@ -38,9 +38,6 @@ namespace SuperMetroidvania5Million.Libraries.Sprite.Projectiles
             Location = Vector2.Add(Location, Direction);
             Space = new Rectangle((int)Location.X, (int)Location.Y, Space.Width, Space.Height);
 
-            //Die if a collision occurs or the projectile leaves the screen
-            isDead = isDead || Location.X > 800 || Location.X < 0 || Location.Y > 480 || Location.Y < 0;
-
             sprite.Update(gameTime);
         }
         public Rectangle SpaceRectangle()
