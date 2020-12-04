17549c14cdd865e3f304afa17cc177fcd1c90bf1
wrfloyd15 <wrfloyd15@gmail.com>
2020-12-04 14:27:29 -0500

    Enemies spawn at correct heights in endless mode

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/EndlessLevel.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/EndlessLevel.cs
@@ -11,10 +11,22 @@ namespace SuperMetroidvania5Million.Libraries.CSV
     public class EndlessLevel : IStageState
     {
         private Game1 game;
+        private List<String> enemyList;
+        private Dictionary<String, int> enemyDict;
 
         public EndlessLevel(Game1 game)
         {
             this.game = game;
+            enemyList = new List<string>() { "Zeela", "Kraid", "Skree", "SideHopper", "Memu" };
+
+            //Make dictionary with y values for the enemies
+            enemyDict = new Dictionary<String, int>();
+            enemyDict.Add("Zeela", 384);
+            enemyDict.Add("Kraid", 352);
+            enemyDict.Add("Skree", 64);
+            enemyDict.Add("SideHopper", 352);
+            enemyDict.Add("Memu", 128);
+
         }
 
         public void LeftDoor(Game1 game)
@@ -54,19 +66,14 @@ namespace SuperMetroidvania5Million.Libraries.CSV
             //Random enemy at random location, y values need to be predetermined and Geega already keeps spawning
 
             //Make a list of possible enemies to spawn in and pick a random one
-            List<String> enemyList = new List<string>(){ "Zeela", "Kraid", "Skree", "SideHopper", "Memu" };
             int index = new Random().Next(0, enemyList.Count);
             String enemy = enemyList[index];
 
-
-
-
-            //Randomly generate location (Map goes from 32 to 
             int xLocation = new Random().Next(64, 736);
 
             if (GameObjectContainer.Instance.EnemyList.Count == 2)
             {
-                EnemyObjectGenerator.Instance.createEnemy(new Vector2(xLocation, 100), enemy, game);
+                EnemyObjectGenerator.Instance.createEnemy(new Vector2(xLocation, enemyDict[enemy]), enemy, game);
             }
         }
 
