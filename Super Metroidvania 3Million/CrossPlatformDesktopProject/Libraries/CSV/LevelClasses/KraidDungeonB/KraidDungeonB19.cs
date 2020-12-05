28550c6dcdfdbc5312404540387de7a7a70b6adf
Albatro5s <49098697+Albatro5s@users.noreply.github.com>
2020-12-05 02:17:52 -0500

    18 19

--- Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/KraidDungeonB/KraidDungeonB19.cs
+++ Super Metroidvania 3Million/CrossPlatformDesktopProject/Libraries/CSV/LevelClasses/KraidDungeonB/KraidDungeonB19.cs
@@ -20,7 +20,7 @@ namespace SuperMetroidvania5Million.Libraries.CSV
         }
         public void TopLeftDoor(Game1 game)
         {
-            LoadCsv.Instance.Load("KraidDungeonB18.csv", new Vector2(1400, 192), game);
+            LoadCsv.Instance.Load("KraidDungeonB18.csv", new Vector2(380, 192), game);
             LevelStatePattern.Instance.state = new KraidDungeonB18();
         }
         public void TopRightDoor(Game1 game)
